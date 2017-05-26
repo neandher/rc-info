6b3b976ab3a7ffd28a31da0d2e6eb44f29367c56
Neandher Carlos <neandher89@gmail.com>
2017-05-26 17:19:32 -0300

    ...

--- web/admin/assets/global/scripts/custom.js
+++ web/admin/assets/global/scripts/custom.js
@@ -1,3 +1,15 @@
+$('#modalConfirmation').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var path = button.data('path');
+    var crf = button.data('crf');
+    var delBtn = $('.delete-resource-form');
+
+    delBtn.attr({
+        'data-path': path,
+        'data-crf': crf
+    });
+});
+
 $('.delete-resource-form').on('click', function () {
 
     var $form = $(document.createElement('form'));
