8c83af9a361f3cb2412a340ea2d4cfa8d04cf32e
Neandher Carlos <neandher89@gmail.com>
2017-09-12 11:04:22 -0300

    ...

--- web/app.php
+++ web/app.php
@@ -17,6 +17,7 @@ if (PHP_VERSION_ID < 70000) {
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
+Request::setTrustedProxies(array($request->server->get('REMOTE_ADDR'))); //to https work
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
