e824070d99b67c5cbfdd356821f5ae60221e6f32
Neandher Carlos <neandher89@gmail.com>
2017-10-27 14:01:47 -0200

    ...

--- web/hook.php
+++ web/hook.php
@@ -2,6 +2,7 @@
 
 $path = '/usr/bin/env -i HOME=/var/www/html/rcinformatica.net.br/web';
 
+exec("$path export SYMFONY_ENV=prod");
 exec("$path git pull origin master");
 exec("$path php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader");
 exec("$path php bin/console cache:clear --env=prod --no-debug --no-warmup");
