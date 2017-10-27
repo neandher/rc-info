bf8662e72f5dae88348e93f9cc21fffa0e5ba0b0
Neandher Carlos <neandher89@gmail.com>
2017-10-27 16:17:08 -0200

    ...

--- web/hook.php
+++ web/hook.php
@@ -1,16 +1,16 @@
 <?php
 
-$mirrordir = "/var/www/html/rcinformatica.net.br/web";
-$gitdir = $mirrordir . "/.git";
+$path = "cd /var/www/html/rcinformatica.net.br/web";
 
-$commands = "/usr/libexec/git-core/git --work-tree=$mirrordir --git-dir=$gitdir pull -f origin master";
-$commands .= " && export SYMFONY_ENV=prod";
-$commands .= " && cd $mirrordir";
+//export SYMFONY_ENV=prod && git pull origin master && php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader && php bin/console cache:clear --env=prod --no-debug --no-warmup && php bin/console cache:clear --env=prod --no-debug --no-warmup && php bin/console cache:warmup --env=prod && php bin/console doctrine:migrations:migrate -n
+
+$commands = "export SYMFONY_ENV=prod";
+$commands .= " && git pull origin master";
 $commands .= " && php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader";
 $commands .= " && php bin/console cache:clear --env=prod --no-debug --no-warmup";
 $commands .= " && php bin/console cache:warmup --env=prod";
 $commands .= " && php bin/console doctrine:migrations:migrate -n";
 
-exec("$commands");
+exec("$path $commands");
 
 echo "All done!";
