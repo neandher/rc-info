292a4670e2c18521fd3b0d4b019d61ca8ea454d7
Neandher Carlos <neandher89@gmail.com>
2017-10-27 13:40:17 -0200

    ...

--- hook.php
+++ hook.php
@@ -3,7 +3,7 @@
 $path = '/usr/bin/env -i HOME=/var/www/html/rcinformatica.net.br/web';
 
 exec("$path git pull origin master");
-//exec("$path composer install --no-dev --optimize-autoloader");
+exec("$path php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader");
 exec("$path php bin/console cache:clear --env=prod --no-debug --no-warmup");
 exec("$path php bin/console cache:warmup --env=prod");
 exec("$path php bin/console assetic:dump --env=prod --no-debug");
