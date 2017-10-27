99128b8b31c59cdd6c358c47a2153cfa9e8f3f52
Neandher Carlos <neandher89@gmail.com>
2017-10-27 13:43:17 -0200

    ...

--- web/hook.php
+++ web/hook.php
@@ -6,7 +6,7 @@ exec("$path git pull origin master");
 exec("$path php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader");
 exec("$path php bin/console cache:clear --env=prod --no-debug --no-warmup");
 exec("$path php bin/console cache:warmup --env=prod");
-exec("$path php bin/console assetic:dump --env=prod --no-debug");
+//exec("$path php bin/console assetic:dump --env=prod --no-debug");
 exec("$path php bin/console doctrine:migrations:migrate -n");
 
 echo "All done!";
