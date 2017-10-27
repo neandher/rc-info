fed68dd95d3d12d0710949787271bda94a863db3
Neandher Carlos <neandher89@gmail.com>
2017-10-27 15:58:11 -0200

    ...

--- web/hook.php
+++ web/hook.php
@@ -1,15 +1,16 @@
-#!/usr/local/bin/php
-<?php #!/usr/bin/env /usr/bin/php
+<?php
 
-$path = "cd /var/www/html/rcinformatica.net.br/web";
+$mirrordir = "/var/www/html/rcinformatica.net.br/web";
+$gitdir = $mirrordir . "/.git";
 
-$commands = "/usr/libexec/git-core/git pull origin master";
+$commands = "/usr/libexec/git-core/git --work-tree=$mirrordir --git-dir=$gitdir pull -f origin master";
 $commands .= " && export SYMFONY_ENV=prod";
+$commands .= " && cd $mirrordir";
 $commands .= " && php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader";
 $commands .= " && php bin/console cache:clear --env=prod --no-debug --no-warmup";
 $commands .= " && php bin/console cache:warmup --env=prod";
 $commands .= " && php bin/console doctrine:migrations:migrate -n";
 
-exec("{$path} {$commands}");
+exec("$commands");
 
 echo "All done!";
