198491ce1e068c82bb2ac78c41a99ee684e2ae15
Neandher Carlos <neandher89@gmail.com>
2017-10-27 14:50:01 -0200

    ...

--- web/hook.php
+++ web/hook.php
@@ -2,12 +2,13 @@
 
 $path = "cd /var/www/html/rcinformatica.net.br/web";
 
-shell_exec("{$path} export SYMFONY_ENV=prod");
-shell_exec("$path git pull origin master");
-shell_exec("$path php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader");
-shell_exec("$path php bin/console cache:clear --env=prod --no-debug --no-warmup");
-shell_exec("$path php bin/console cache:warmup --env=prod");
-//exec("$path php bin/console assetic:dump --env=prod --no-debug");
-shell_exec("$path php bin/console doctrine:migrations:migrate -n");
+$commands = "export SYMFONY_ENV=prod";
+$commands .= " && git pull origin master";
+$commands .= " && php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader";
+$commands .= " && php bin/console cache:clear --env=prod --no-debug --no-warmup";
+$commands .= " && php bin/console cache:warmup --env=prod";
+$commands .= " && php bin/console doctrine:migrations:migrate -n";
+
+shell_exec("{$path} {$commands}");
 
 echo "All done!";
