a3ff6c1a9d3a2a2691b449b28ab618b02ed5d92f
Neandher Carlos <neandher89@gmail.com>
2017-10-27 15:31:48 -0200

    ...

--- web/hook.php
+++ web/hook.php
@@ -2,11 +2,13 @@
 
 $path = "cd /var/www/html/rcinformatica.net.br/web";
 
-$commands = "export SYMFONY_ENV=prod";
-$commands .= " && git pull origin master";
+$commands = "/usr/libexec/git-core/git pull origin master";
+$commands .= " && export SYMFONY_ENV=prod";
 $commands .= " && php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader";
 $commands .= " && php bin/console cache:clear --env=prod --no-debug --no-warmup";
 $commands .= " && php bin/console cache:warmup --env=prod";
 $commands .= " && php bin/console doctrine:migrations:migrate -n";
 
-exec("/var/www/html/rcinformatica.net.br/web/web/master_deploy.sh");
+exec("{$path} {$commands}");
+
+echo "All done!";
