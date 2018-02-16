2b68c55617f659f9ca0d4f738626c21f5df52780
Neandher Carlos <neandher89@gmail.com>
2018-02-16 13:33:26 -0200

    fix banner

--- web/hook.php
+++ web/hook.php
@@ -2,7 +2,7 @@
 
 $path = "cd /var/www/html/rcinformatica.net.br/web";
 
-//export SYMFONY_ENV=prod && git pull origin master && php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader && php bin/console cache:clear --env=prod --no-debug --no-warmup && php bin/console cache:clear --env=prod --no-debug --no-warmup && php bin/console cache:warmup --env=prod && php bin/console doctrine:migrations:migrate -n
+//export SYMFONY_ENV=prod && git pull origin master && php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader && php bin/console cache:clear --env=prod --no-debug --no-warmup && php bin/console cache:warmup --env=prod && php bin/console doctrine:migrations:migrate -n
 
 $commands = "export SYMFONY_ENV=prod";
 $commands .= " && git pull origin master";
